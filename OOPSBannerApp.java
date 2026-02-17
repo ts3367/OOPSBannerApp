905188774cfbf9e25056af01439910d7829f6b32
boredlol0 <toshi.saravanan@icloud.com>
2026-02-17 14:13:24 +0530

    Initial Commit

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,23 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("   ***    " + "   ***    " + "  ******  " + "  *****   ");
+
+        // Row 2
+        System.out.println("  **   **  " + "  **   **  " + "  **    ** " + " **    **  ");
+
+        // Row 3
+        System.out.println(" **     ** " + " **     ** " + "  **    ** " + " **        ");
+
+        // Row 4
+        System.out.println(" **     ** " + " **     ** " + "  ******   " + "  *****    ");
+
+        // Row 5
+        System.out.println(" **     ** " + " **     ** " + "  **       " + "       **  ");
+
+        // Row 6
+        System.out.println("  **   **  " + "  **   **  " + "  **       " + " **    **  ");
+
+        // Row 7
+        System.out.println("   ***    " + "   ***    " + "  **       " + "  *****    ");
     }
 }
