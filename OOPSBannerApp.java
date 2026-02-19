3ed46d9fc1406ee4f2ac471e92979bfd1cfe295a
boredlol0 <toshi.saravanan@icloud.com>
2026-02-19 19:11:52 +0530

    print using string join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,10 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("   ***    " + "   ***    " + "  ******  " + "  *****   ");
 
-        // Row 2
-        System.out.println("  **   **  " + "  **   **  " + "  **    ** " + " **    **  ");
-
-        // Row 3
-        System.out.println(" **     ** " + " **     ** " + "  **    ** " + " **        ");
-
-        // Row 4
-        System.out.println(" **     ** " + " **     ** " + "  ******   " + "  *****    ");
-
-        // Row 5
-        System.out.println(" **     ** " + " **     ** " + "  **       " + "       **  ");
-
-        // Row 6
-        System.out.println("  **   **  " + "  **   **  " + "  **       " + " **    **  ");
-
-        // Row 7
-        System.out.println("   ***    " + "   ***    " + "  **       " + "  *****    ");
+        System.out.println(String.join(" ", " **** ", " **** ", " ***** ", " ***** "));
+        System.out.println(String.join(" ", "**  **", "**  **", "**   **", "**     "));
+        System.out.println(String.join(" ", "**  **", "**  **", "*****  ", " ****  "));
+        System.out.println(String.join(" ", "**  **", "**  **", "**     ", "     **"));
+        System.out.println(String.join(" ", " **** ", " **** ", "**     ", " ***** "));
     }
 }
