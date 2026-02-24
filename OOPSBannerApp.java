31297839db62d13fe898fc8f571525d2218e5002
boredlol0 <toshi.saravanan@icloud.com>
2026-02-24 14:37:40 +0530

    initial commit

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,16 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ", " **** ", " **** ", " ***** ", " ***** "));
-        System.out.println(String.join(" ", "**  **", "**  **", "**   **", "**     "));
-        System.out.println(String.join(" ", "**  **", "**  **", "*****  ", " ****  "));
-        System.out.println(String.join(" ", "**  **", "**  **", "**     ", "     **"));
-        System.out.println(String.join(" ", " **** ", " **** ", "**     ", " ***** "));
+        String[] lines = new String[5];
+
+        lines[0] = String.join(" ", " **** ", " **** ", " ***** ", " ***** ");
+        lines[1] = String.join(" ", "**  **", "**  **", "**   **", "**     ");
+        lines[2] = String.join(" ", "**  **", "**  **", "*****  ", " ****  ");
+        lines[3] = String.join(" ", "**  **", "**  **", "**     ", "     **");
+        lines[4] = String.join(" ", " **** ", " **** ", "**     ", " ***** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
